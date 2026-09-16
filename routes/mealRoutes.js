77660bd58569ff454c3ec56e7eaf58bcd8b5e54b
PraveenKumar S <praveenkumar@katenterprise.com>
2026-09-16 19:15:33 +0530

    updates on the structure

--- routes/mealRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMeals } = require('../controllers/mealController');
-
-router.get('/', getMeals);
-
-module.exports = router;
